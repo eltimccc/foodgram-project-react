de6f657cdb6801a60843cbb342e8fb351d68a572
Денис <eltimccc@yandex.ru>
2022-11-24 17:20:42 +0300

    wer

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME", default='postgres'),
         "USER": os.getenv("POSTGRES_USER", default='postgres'),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default='postgres'),
-        "HOST": os.getenv("DB_HOST", default='db'),
+        "HOST": os.getenv("DB_HOST", default='84.201.128.143'),
         "PORT": os.getenv("DB_PORT", default='5432')
     }
 }
