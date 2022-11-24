1cd3035fad17458fb117b49afc019bb4109628c7
Денис <eltimccc@yandex.ru>
2022-11-24 17:30:11 +0300

    23434

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-# 123456789010111234567891
+# 1234567890101112345678912
