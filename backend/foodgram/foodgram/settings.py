c030b18b221011c8610fbec28db60d19cc4c04eb
Денис <eltimccc@yandex.ru>
2022-11-27 16:23:08 +0300

    Картинки должны заработать когда-нибудь.

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -137,8 +137,6 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_URL = "/static/"
-
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
